33bb4ea89a4773d3832de352c21fe1475361d083
STRIPS\jurema <jure.macerll@gmail.com>
2017-10-19 15:46:29 +0200

    cmdline, config

--- initial_setup/rpi_setup.py
+++ initial_setup/rpi_setup.py
@@ -1,60 +1,60 @@
 import os
 
-# update all
-os.system("sudo apt-get update")
-os.system("sudo apt-get upgrade -y")
-
-# install python virtual venv
-os.system("sudo apt-get -y install python3-venv")
-os.system("sudo python3 -m venv /venv_strips_tester")
-os.system("virtualenv -p /venv_strips_tester/bin/python3.4 /venv_strips_tester")
-os.system("sudo chown -R pi /venv_strips_tester/")
-
-###hidapi dependencies
-os.system("sudo apt-get install -y libusb-1.0-0-dev")
-os.system("sudo apt-get install -y python-dev")
-os.system("sudo apt-get install -y libudev-dev")
-os.system("sudo apt-get install -y libtool")
-
-### build shared library
-os.system("sudo apt-get install -y autoconf")
-os.system("sudo apt-get install -y build-essential")
-os.system("sudo apt-get install -y libpq-dev")
-os.system("git clone https://github.com/signal11/hidapi.git /home/pi/Desktop/hidapi")
-os.system("/bin/bash /home/pi/Desktop/hidapi/bootstrap")
-os.system("/bin/bash /home/pi/Desktop/hidapi/configure")
-os.system("cd /home/pi/Desktop/hidapi")
-os.system("ls")
-os.system("sudo make")
-os.system("sudo make install")
-#os.system("/venv_strips_tester/bin/python -m pip install hidapi")
-
-# QR code
-os.system("sudo apt-get install -y libdmtx0a")
-#os.system("/venv_strips_tester/bin/python -m pip install pylibdmtx")
-
-## I2C
-os.system("sudo apt-get install -y i2c-tools")
-#os.system("sudo cp /strips_tester_project/initial_setup/modules /etc/modules")
-#os.system("/venv_strips_tester/bin/python -m pip install smbus2") # to requirements
-
-### postgresql
-os.system("sudo apt-get install -y libpq-dev")
-os.system("sudo apt-get install -y postgresql-9.4")
-
-# python
-os.system("/venv_strips_tester/bin/python -m pip install pip -UI")
-os.system("/venv_strips_tester/bin/python -m pip install setuptools -UI")
-os.system("/venv_strips_tester/bin/python -m pip install -r /strips_tester_project/initial_setup/requirements.txt")
-os.system("sudo cp /strips_tester_project/initial_setup/python-sudo.sh /venv_strips_tester/bin/")
+# # update all
+# os.system("sudo apt-get update")
+# os.system("sudo apt-get upgrade -y")
+#
+# # install python virtual venv
+# os.system("sudo apt-get -y install python3-venv")
+# os.system("sudo python3 -m venv /venv_strips_tester")
+# os.system("virtualenv -p /venv_strips_tester/bin/python3.4 /venv_strips_tester")
+# os.system("sudo chown -R pi /venv_strips_tester/")
+#
+# ###hidapi dependencies
+# os.system("sudo apt-get install -y libusb-1.0-0-dev")
+# os.system("sudo apt-get install -y python-dev")
+# os.system("sudo apt-get install -y libudev-dev")
+# os.system("sudo apt-get install -y libtool")
+#
+# ### build shared library
+# os.system("sudo apt-get install -y autoconf")
+# os.system("sudo apt-get install -y build-essential")
+# os.system("sudo apt-get install -y libpq-dev")
+# os.system("git clone https://github.com/signal11/hidapi.git /home/pi/Desktop/hidapi")
+# os.system("/bin/bash /home/pi/Desktop/hidapi/bootstrap")
+# os.system("/bin/bash /home/pi/Desktop/hidapi/configure")
+# os.system("cd /home/pi/Desktop/hidapi")
+# os.system("ls")
+# os.system("sudo make")
+# os.system("sudo make install")
+# #os.system("/venv_strips_tester/bin/python -m pip install hidapi")
+#
+# # QR code
+# os.system("sudo apt-get install -y libdmtx0a")
+# #os.system("/venv_strips_tester/bin/python -m pip install pylibdmtx")
+#
+# ## I2C
+# os.system("sudo apt-get install -y i2c-tools")
+# #os.system("sudo cp /strips_tester_project/initial_setup/modules /etc/modules")
+# #os.system("/venv_strips_tester/bin/python -m pip install smbus2") # to requirements
+#
+# ### postgresql
+# os.system("sudo apt-get install -y libpq-dev")
+# os.system("sudo apt-get install -y postgresql-9.4")
+#
+# # python
+# os.system("/venv_strips_tester/bin/python -m pip install pip -UI")
+# os.system("/venv_strips_tester/bin/python -m pip install setuptools -UI")
+# os.system("/venv_strips_tester/bin/python -m pip install -r /strips_tester_project/initial_setup/requirements.txt")
+# os.system("sudo cp /strips_tester_project/initial_setup/python-sudo.sh /venv_strips_tester/bin/")
 
 # rpi files config
-#os.system("sudo systemctl stop serial-getty@ttyS0.service")
-#os.system("sudo systemctl disable serial-getty@ttyS0.service")
-#os.system("sudo rm /boot/cmdline.txt")
-#os.system("sudo cp /strips_tester_project/initial_setup/cmdline.txt /boot/cmdline.txt")
-#os.system("sudo rm /boot/config.txt")
-#os.system("sudo cp /strips_tester_project/initial_setup/config.txt /boot/config.txt")
+os.system("sudo systemctl stop serial-getty@ttyS0.service")
+os.system("sudo systemctl disable serial-getty@ttyS0.service")
+os.system("sudo rm /boot/cmdline.txt")
+os.system("sudo cp /strips_tester_project/initial_setup/cmdline.txt /boot/cmdline.txt")
+os.system("sudo rm /boot/config.txt")
+os.system("sudo cp /strips_tester_project/initial_setup/config.txt /boot/config.txt")
 
 
 # postgresql
