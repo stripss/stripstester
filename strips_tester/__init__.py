43432e92568b2ada89cd4c27f588580813b43233
Strips//Marcel <marcel.jancar@gmail.com>
2019-09-19 15:27:49 +0200

    Fixed missing "

--- strips_tester/__init__.py
+++ strips_tester/__init__.py
@@ -28,7 +28,7 @@ DEBUG = logging.DEBUG
 NOTSET = logging.NOTSET
 
 # RemoteDB address
-remoteDB = "172.30.129.19:27017
+remoteDB = "172.30.129.19:27017"
 
 def initialize_logging(level: int = logging.INFO):
     lgr = logging.getLogger(name=__name__)
